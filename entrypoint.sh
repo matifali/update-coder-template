7d602ffe4cd441a9f29043717c8b6d7e7fc6e898
Muhammad Atif Ali <me@matifali.dev>
2023-06-05 00:54:37 +0300

    support `--activate` flag to push inactive templates

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 # Check if CODER_SESSION_TOKEN is set
 : "${CODER_SESSION_TOKEN:?Variable not set or empty}"
 
-echo "Pushing ${CODER_TEMPLATE_NAME} to ${CODER_URL}..."
+# Check if CODER_ACCESS_URL is set
+: "${CODER_ACCESS_URL:?Variable not set or empty}"
+
+echo "Pushing ${CODER_TEMPLATE_NAME} to ${CODER_ACCESS_URL}..."
 
 # if the CODRR_TEMPLATE_DIR is empty string, then use the TEMPLATE_NAME as the directory
 if [ -z "${CODER_TEMPLATE_DIR}" ]; then
@@ -14,7 +17,15 @@ fi
 # if the CODER_TEMPLATE_VERSION is empty string then let coder use a random name
 if [ -z "${CODER_TEMPLATE_VERSION}" ];
 then
-    coder templates push ${CODER_TEMPLATE_NAME} --directory ./${CODER_TEMPLATE_DIR} --url ${CODER_URL} --yes
+    echo "No version specified, using random name."
+    coder templates push ${CODER_TEMPLATE_NAME} --directory ./${CODER_TEMPLATE_DIR} --activate=${CODER_TEMPLATE_ACTIVATE} --yes
 else
-    coder templates push ${CODER_TEMPLATE_NAME} --directory ./${CODER_TEMPLATE_DIR} --url ${CODER_URL} --name ${CODER_TEMPLATE_VERSION} --yes
+    coder templates push ${CODER_TEMPLATE_NAME} --directory ./${CODER_TEMPLATE_DIR} --name ${CODER_TEMPLATE_VERSION} --activate=${CODER_TEMPLATE_ACTIVATE} --yes
+fi
+
+if [ "${CODER_TEMPLATE_ACTIVATE}" == "true" ];
+then
+    echo "Template ${CODER_TEMPLATE_NAME} pushed to ${CODER_ACCESS_URL} and activated."
+else
+    echo "Template ${CODER_TEMPLATE_NAME} pushed to ${CODER_ACCESS_URL}.
 fi
